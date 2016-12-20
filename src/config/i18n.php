c28c83245d3a46fb3210d44fb9c02532e314806f
tofid <andreyklochok@gmail.com>
2016-12-20 20:04:45 +0200

    Added i18n config

---
+++ src/config/i18n.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Agency Theme for hiqdev/yii2-thememanager
+ *
+ * @link      https://github.com/hiqdev/yii2-theme-agency
+ * @package   yii2-theme-agency
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'languages' => ['ru'],
+    'sourcePath' => dirname(__DIR__),
+    'messagePath' => dirname(__DIR__) . '/messages',
+    'sort' => true,
+    'removeUnused' => false,
+];
